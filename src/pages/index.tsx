56f125b8c1c20a5b3fc20d1464e77dc612722620
Adler Web3 <adler.web3@gmail.com>
2022-11-09 20:30:36 -0300

    ⚽[GOAL] Appling Tailwind Styles on HTML

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,40 +11,58 @@ import iconCheckImg from '../assets/icon-check.svg';
 
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-[1124px] mx-auto h-screen grid grid-cols-2 items-center gap-28" >
       <main>
         <Image src={logoImg} alt="NLW Copa Logo" />
 
-        <h1>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
+        <h1 className="mt-14 font-bold text-white text-5xl leading-tight">
+          Crie seu próprio bolão da copa e compartilhe entre amigos!
+        </h1>
 
-        <div>
+        <div className="mt-10 flex items-center gap-2">
           <Image src={usersAvatarExampleImg} alt="" />
-          <strong>
-            <span>+12.592</span> pessoas já estão usando
+          <strong className="text-gray-100 text-xl">
+            <span className="text-ignite-500" >+12.592</span> pessoas já estão usando
           </strong>
         </div>
 
-        <form>
-          <input type="text" required placeholder="Qual nome do seu bolão?" />
-          <button type="submit" >Criar meu bolão</button>
+        <form className="mt-10 flex gap-2">
+          <input
+            className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm text-gray-200"
+            type="text"
+            required
+            placeholder="Qual nome do seu bolão?"
+          />
+
+          <button
+            className="bg-yellow-500 px-6 py-4 rounded text-sm font-bold text-gray-950 uppercase hover:bg-yellow-700"
+            type="submit"
+          >
+            Criar meu bolão
+          </button>
         </form>
 
-        <p>Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀</p>
+        <p className="mt-4 text-sm text-gray-300 leading-relaxed">
+          Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀
+        </p>
 
-        <div>
 
-          <div>
+        <div className="mt-10 pt-10 border-t border-gray-600 flex justify-between items-center text-gray-100">
+
+          <div className="flex items-center gap-6">
             <Image src={iconCheckImg} alt="" />
-            <div>
-              <span>+2.034</span>
+            <div className="flex flex-col ">
+              <span className="font-bold text-2xl">+2.034</span>
               <span>Bolões criados</span>
             </div>
           </div>
 
-          <div>
+          <div className="w-px h-14 bg-gray-600" />
+
+          <div className="flex items-center gap-6">
             <Image src={iconCheckImg} alt="" />
-            <div>
-              <span>+192.847</span>
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl">+192.847</span>
               <span>Palpites enviados</span>
             </div>
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,27 @@ module.exports = {
       fontFamily: {
         sans: 'Roboto, sans-serif'
       },
+
+      backgroundImage: {
+        app: 'url(/app-bg.png)'
+      },
+
       colors: {
+        ignite: {
+          500: '#129E57'
+        },
+        yellow: {
+          500: '#F7DD43',
+          700: '#BBA733',
+        },
         gray: {
-          900: '#121214'
+          100: '#E1E1E6',
+          200: '#C4C4CC',
+          300: '#8D8D99',
+          600: '#323238',
+          800: '#202024',
+          900: '#121214',
+          950: '#09090A',
         }
       }
     },
